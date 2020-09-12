e43e4a4b6ea748b231acbed87367ed8613073d64
shrik450 <shrik450@gmail.com>
2020-09-12 18:09:38 +0530

    Finish post show page

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ gem 'jbuilder', '~> 2.7'
 gem 'bootsnap', '>= 1.4.2', require: false
 gem "sorbet-runtime"
 gem "sorbet-rails"
+gem "sprockets-rails", require: "sprockets/railtie"
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,7 @@
 # typed: strict
 class Comment < ApplicationRecord
+  # <tt>optional: true</tt> is necessary for now since there's comments from months
+  # there are no posts from.
   belongs_to :parent, polymorphic: true, inverse_of: :comments, optional: true
   has_many :comments, as: :parent
 end
