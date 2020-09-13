38ace1dc84cb28f1b5726264bd68e3807ced1fe9
shrik450 <shrik450@gmail.com>
2020-09-13 12:00:20 +0530

    Add login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  skip_before_action :authenticate
+
   def new
     user = User.find_by(email: params[:email])
     if user.present? && user.authenticate(params[:password])
