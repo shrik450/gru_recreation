2bb6a2b82d2db91c5f7d18be0a194b7360bbe804
shrik450 <shrik450@gmail.com>
2020-09-13 15:31:14 +0530

    Add admin column for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_13_070956) do
+ActiveRecord::Schema.define(version: 2020_09_13_100031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,6 +92,7 @@ ActiveRecord::Schema.define(version: 2020_09_13_070956) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "codes", "users"
