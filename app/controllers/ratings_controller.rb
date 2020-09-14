cc71b588ffd41d573f304853fe8a3d4d02d94d4a
shrik450 <shrik450@gmail.com>
2020-09-14 09:54:59 +0530

    Create rating page

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,4 +1,22 @@
 class RatingsController < ApplicationController
   def create
+    redirect_path =
+      if params[:next_post_id].present?
+        posts_rate_path(params[:next_post_id])
+      else
+        root_path
+      end
+
+    if Rating.create(rating_params)
+      redirect_to redirect_path
+    else
+      alert.flash("Invalid")
+    end
+  end
+
+  private
+
+  def rating_params
+    params.require(:rating).permit(:user_id, :post_id, :rating)
   end
 end
