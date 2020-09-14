f90608d5da16466e98af96eb2a3e5c613b77a393
shrik450 <shrik450@gmail.com>
2020-09-14 09:47:17 +0530

    Remove attempts at typing controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,9 @@
-# typed: true
+# typed: ignore
 class PostsController < ApplicationController
   before_action :set_post, except: %i[index rate_index]
 
   def index
-    if T.must(current_user).admin?
+    if current_user.admin?
       @q = Post.ransack(params[:q])
       @posts = @q.result.page(params[:page])
       render "index"
@@ -13,18 +13,18 @@ class PostsController < ApplicationController
   end
 
   def show
-    unless T.must(current_user).admin?
+    unless current_user.admin?
       render_forbidden
     end
   end
 
   def rate
-    @next_post = Post.random_top_100_post_unrated_by(T.must(current_user))
+    @next_post = Post.random_top_100_post_unrated_by(current_user)
     render "rate"
   end
 
   def rate_index
-    unrated_post = Post.random_top_100_post_unrated_by(T.must(current_user))
+    unrated_post = Post.random_top_100_post_unrated_by(current_user)
     if unrated_post.present?
       redirect_to posts_rate_path(unrated_post.id)
     else
