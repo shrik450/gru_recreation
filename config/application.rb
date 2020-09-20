15ae5d032168104bf0e87d449878a639fdfc0374
D3nj4l <d3nj4l@protonmail.com>
2020-09-20 09:39:31 +0000

    Change study period to be through November

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ require "action_view/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-STUDY_MONTHS = T.let(("01".."12").map {|month| "2019-#{month}"}.freeze, T::Array[String])
+STUDY_MONTHS = T.let(("01".."11").map {|month| "2019-#{month}"}.freeze, T::Array[String])
 
 module GruRecreate
   class Application < Rails::Application
